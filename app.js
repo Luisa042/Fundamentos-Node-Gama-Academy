77a416b46818f1785b6cae75cf383471935c3218
Luisa <luisa.santos042@gmail.com>
2021-08-21 18:39:41 -0300

    implementa CRUD

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
 const http = require('http');
 const url = require('url');
 const queryString = require('query-string');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 // business rule
 const server = http.createServer((req, res) => {
-  // retrieve 'question' on url
-  const params = queryString.parse(url.parse(req.url, true).search);
+  var reply;
+  const urlParse = url.parse(req.url, true);
 
-  // verifies 'question' and choose 'answer'
-  let answer;
-  if (params.question == 'meaning-of-life') {
-    answer = '42';
-  } else if (params.question == 'foo') {
-    answer = 'bar';
-  } else {
-    answer = "don't know";
+  // - receive user info
+  const params = queryString.parse(urlParse.search);
+
+  // CREATE user:
+  if (urlParse.pathname == '/create-user') {
+    // - save user info
+    fs.appendFile(
+      'users/' + params.id + '.txt',
+      JSON.stringify(params),
+      function (err) {
+        if (err) throw err;
+      }
+    );
+    reply = 'user created';
+    res.statusCode = 201;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(reply);
   }
 
-  // returns chosen answer
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(answer);
+  // READ user
+  else if (urlParse.pathname == '/get-user') {
+    // - show user info
+    fs.readFile('users/' + params.id + '.txt', function (err, data) {
+      if (err) throw err;
+      reply = data;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(reply);
+    });
+  }
+
+  // UPDATE user
+  else if (urlParse.pathname == '/update-user') {
+    // - modify user info
+    fs.writeFile(
+      'users/' + params.id + '.txt',
+      JSON.stringify(params),
+      function (err) {
+        if (err) throw err;
+      }
+    );
+    reply = 'user updated';
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(reply);
+  }
+  // DELETE user
+  else if (urlParse.pathname == '/delete-user') {
+    fs.unlink('users/' + params.id + '.txt', function (err) {
+      reply = err ? 'user not found' : 'user deleted';
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(reply);
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
